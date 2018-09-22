c8e0beb63fa4a36fd6e7ccdcbf52240b35e67fa0
lastlink <theefunk@gmail.com>
2018-09-22 13:04:08 -0400

    update docs

--- js/leftSide.js
+++ js/leftSide.js
@@ -14,8 +14,19 @@ $( document ).ready(function() {
 console.log("test")
 
 $( "#leftMap" ).click(function() {
-	alert( "Handler for leftMap .click() called." );
-	console.log("leftMap")
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://gameral.com/api/api.php/t_state/1",
+		"method": "GET",
+		"headers": {}
+	  }
+	  
+	  $.ajax(settings).done(function (response) {
+		console.log(response);
+	  });
+	// alert( "Handler for leftMap .click() called." );
+	// console.log("leftMap")
 
   });
 
