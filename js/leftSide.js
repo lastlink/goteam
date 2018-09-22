244581afa1c93f91b423700a182341a1554633e0
lastlink <theefunk@gmail.com>
2018-09-22 13:37:59 -0400

    dyn slider

--- js/leftSide.js
+++ js/leftSide.js
@@ -1,65 +1,92 @@
-$( document ).ready(function() {
-	$('#ex1').slider({
-		formatter: function(value) {
-			return 'Current value: ' + value;
-		}
-	});
-	
+$(document).ready(function () {
+	// $('#ex1').slider({
+	// 	formatter: function(value) {
+	// 		return 'Current value: ' + value;
+	// 	}
+	// });
+
 	// Without JQuery
 	// var slider = new Slider('#ex1', {
 	// 	formatter: function(value) {
 	// 		return 'Current value: ' + value;
 	// 	}
 	// });
-console.log("test")
-var leftStateData={};
+	console.log("test")
+	var leftStateData = {};
 
-$( "#leftMap" ).click(function() {
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "http://gameral.com/api/api.php/t_state/1",
-		"method": "GET",
-		"headers": {}
-	  }
-	  
-	  $.ajax(settings).done(function (response) {
-		  console.log("response")
-		// console.log(response);
-
-		var  leftStateData=JSON.parse(response);
-		leftStateData.issues=[]
-		// console.log("issues")
-
-		// console.log(leftStateData.issues);
-
-		var issueSize = 6;
-
-		for (let i = 1; i <= issueSize; i++) {
-			var issue={
-				label:leftStateData["issue"+i],
-				value:leftStateData["value"+i],
-				toplic:leftStateData["topic"+i]
-			}
-			leftStateData.issues.push(issue)
-
+	$("#leftMap").click(function () {
+		var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "http://gameral.com/api/api.php/t_state/1",
+			"method": "GET",
+			"headers": {}
 		}
-		
-		// response.issues.
-		console.log("leftStateData:")
 
-		console.log(leftStateData);
+		$.ajax(settings).done(function (response) {
+			console.log("response")
+			// console.log(response);
 
-		$( "#leftPopup" ).toggle();
-	  });
-	// alert( "Handler for leftMap .click() called." );
-	// console.log("leftMap")
+			var leftStateData = JSON.parse(response);
+			leftStateData.issues = []
+			// console.log("issues")
 
-  });
+			// console.log(leftStateData.issues);
 
-  $( "#rightMap" ).click(function() {
-	alert( "Handler for rightMap .click() called." );
-	console.log("rightMap")
+			var issueSize = 6;
 
-  });
+			for (let i = 1; i <= issueSize; i++) {
+				var issue = {
+					label: leftStateData["issue" + i],
+					value: leftStateData["value" + i],
+					toplic: leftStateData["topic" + i]
+				}
+				leftStateData.issues.push(issue)
+
+			}
+
+			// response.issues.
+			console.log("leftStateData:")
+
+			console.log(leftStateData);
+			// add inputs
+			// 	<!-- 
+			var proposalHtml = "Proposal:"
+			for (let i = 0; i < leftStateData.issues.length; i++) {
+				const element = leftStateData.issues[i];
+
+				proposalHtml += `
+			<h3>`+ element.label + `</h3>
+     <br> 
+	  <input id="issue`+ i + 1 + `" width:'100%' data-slider-id='issue` + i + 1 + `Slider' type="text" data-slider-min="0" data-slider-max="` + element.value * 2 + `" data-slider-step="1" data-slider-value="` + element.value + `"/>
+	  <br> 
+			`
+
+			}
+
+
+			$("#proposal")
+				.html(proposalHtml);
+
+			for (let i = 0; i < leftStateData.issues.length; i++) {
+				var slider = new Slider('#issue' + i + 1, {
+					formatter: function (value) {
+						return 'Current value: ' + value;
+					}
+				});
+			}
+
+
+			$("#leftPopup").toggle();
+		});
+		// alert( "Handler for leftMap .click() called." );
+		// console.log("leftMap")
+
+	});
+
+	$("#rightMap").click(function () {
+		alert("Handler for rightMap .click() called.");
+		console.log("rightMap")
+
+	});
 })
