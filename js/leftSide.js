cfe7d36fc2dcdebac252414bd7c731cd67ec6d62
lastlink <theefunk@gmail.com>
2018-09-22 12:43:25 -0400

    some changes

--- js/leftSide.js
+++ js/leftSide.js
@@ -1,1 +1,15 @@
+$( document ).ready(function() {
 console.log("test")
+
+$( "#leftMap" ).click(function() {
+	alert( "Handler for leftMap .click() called." );
+	console.log("leftMap")
+
+  });
+
+  $( "#rightMap" ).click(function() {
+	alert( "Handler for rightMap .click() called." );
+	console.log("rightMap")
+
+  });
+})
